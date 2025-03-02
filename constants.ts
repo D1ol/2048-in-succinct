ef2fbc293de051a1acc5a267117c9e5fccf88a60
Ihor Shchypakov <>
2025-03-02 13:35:11 +0100

    initial commit

--- constants.ts
+++ constants.ts
@@ -20,15 +20,15 @@ export const moveAnimationDuration = 150; // ms
 export const gameWinTileValue = process.env.NEXT_PUBLIC_WIN_TILE_VALUE || 2048;
 
 export const gameTileImages: { [key: number]: string } = {
-  2: "/tiles/snake2.jpg",
-  4: "/tiles/dino4.jpg",
-  8: "/tiles/frog8.jpg",
-  16: "/tiles/ape16.jpg",
-  32: "/tiles/boar32.jpg",
-  64: "/tiles/whale64.jpg",
-  128: "/tiles/chad128.jpg",
-  256: "/tiles/piggy256.png",
-  512: "/tiles/piggy512.png",
-  1024: "/tiles/piggy1024.png",
-  2048: "/tiles/piggy2048.png",
+  2: "/tiles/2.png",
+  4: "/tiles/4.png",
+  8: "/tiles/8.png",
+  16: "/tiles/16.png",
+  32: "/tiles/32.png",
+  64: "/tiles/64.png",
+  128: "/tiles/128.png",
+  256: "/tiles/256.png",
+  512: "/tiles/512.png",
+  1024: "/tiles/1024.png",
+  2048: "/tiles/2048.png",
 };
